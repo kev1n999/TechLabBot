db37384e503201aba90dfe12eb42d6d2f7eb62bf
kev1n999 <sopratesta999@gmail.com>
2025-08-30 11:12:54 -0300

    feat: add ban command

--- src/app/commands/private/ban.py
+++ src/app/commands/private/ban.py
@@ -9,7 +9,7 @@ class BanCommand(SlashCommandBuilder):
             description="Bane um membro do servidor"
         )
         
-    async def callback(self, interaction: discord.Interaction, member: discord.Member):
+    async def callback(self, interaction: discord.Interaction, member: discord.Member, reason: str=None):
         if not interaction.user.guild_permissions.administrator:
             await interaction.response.send_message(
                 content="Você não possui as permissções necessárias para a execução deste comando!",
@@ -17,4 +17,35 @@ class BanCommand(SlashCommandBuilder):
             )
             return 
         
+        if member.guild_permissions.value >= interaction.user.guild_permissions.value:
+            await interaction.response.send_message(
+                content="Você não pode banir um membro que tenha permissões superiores ou iguais as suas!",
+                ephemeral=True 
+            )
+            return 
         
+        try:
+            await member.ban(
+                reason=reason 
+            )
+            
+            if reason is None:
+                reason = "Sem motivo"
+            
+            try:
+                await member.send(
+                    content=f"Você foi banido do servidor **{interaction.guild.name}** por **{interaction.user.name}**\nMotivo: `{reason}`"
+                )    
+            except:
+                print(f"Não foi possível enviar uma mensagem ao membro banido: {member.name}")
+                
+            await interaction.response.send_message(
+                content=f"{member.mention} foi banido com sucesso do servidor.\nMotivo: **{reason}**",
+                ephemeral=True 
+            )
+            
+        except:
+            await interaction.response.send_message(
+                content="Ocorreu um erro ao banir este usuário.",
+                ephemeral=True 
+            )
